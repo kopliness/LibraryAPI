f96ff57a4e25e07906805eec22cf6e5010b60eb3
Kopliness <mr.vladlis3568@gmail.com>
2023-08-24 19:37:37 +0300

    created book and user models, added application layers

--- Library.Domain/Models/BookModel.cs
+++ Library.Domain/Models/BookModel.cs
@@ -2,6 +2,18 @@
 {
     public class BookModel
     {
+        public Guid Id { get; init; }
 
+        public string Isbn { get; init; }
+
+        public string Title { get; init; }
+
+        public string Description { get; init; }
+
+        public string Author { get; init; }
+
+        public DateTime BorrowTime { get; init; }
+
+        public DateTime ReturnTime { get; init; }
     }
 }
