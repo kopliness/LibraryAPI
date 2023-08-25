a202fbc12cdc9d32b13036e5a46bc1533873f7b6
Kopliness <mr.vladlis3568@gmail.com>
2023-08-25 21:20:58 +0300

    added repository interfaces

---
+++ Library.Database/Repository/Interfaces/IBookRepository.cs
@@ -0,0 +1,14 @@
+﻿using Library.Domain.Dto;
+using Library.Domain.Models;
+
+namespace Library.Database.Repository.Interfaces
+{
+    public interface IBookRepository
+    {
+        Task<BookModel?> AddBookAsync(BookDto bookDto, CancellationToken cancellationToken = default);
+        List<BookModel> GetBooks();
+        Task<BookDto?> GetBookAsync(Guid? id = null, string? isbn = null, CancellationToken cancellationToken = default);
+        Task<BookDto?> UpdateBookAsync(Guid id, BookDto bookDto, CancellationToken cancellationToken = default);
+        Task<BookDto?> DeleteBookAsync(Guid? id = null, string? isbn = null, CancellationToken cancellationToken = default);
+    }
+}
